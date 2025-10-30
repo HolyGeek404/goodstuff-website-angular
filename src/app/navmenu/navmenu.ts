dd8abe1e448a0320f99e8c1a6c8280bc3be7d3c5
HolyGeek404 <wmatkowski404@proton.me>
2025-10-30 18:38:35 +0100

    Base config for calling Azure Func

--- src/app/navmenu/navmenu.ts
+++ src/app/navmenu/navmenu.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-navmenu',
+  standalone: true,
   imports: [],
   templateUrl: './navmenu.html',
   styleUrl: './navmenu.css'
 })
-export class Navmenu {
+export class Navmenu implements OnInit {
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // this.http.get('/api/HelloWorld', { responseType: 'text' })
+    //   .subscribe(resp => console.log(resp));
+  }
 }
