dcc67fc61076150b482d1c567743da290b72190e
HolyGeek404 <wmatkowski404@proton.me>
2025-11-17 11:56:18 +0100

    Removed link's default style

--- src/app/nav/nav.ts
+++ src/app/nav/nav.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    RouterLink
   ],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
