dc08cbc18b9f32aeaf2745fd600bbefcee810d50
HolyGeek404 <wmatkowski404@proton.me>
2026-03-19 16:40:39 +0100

    working on adding PrimeNG ui library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,18 +2,12 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import { routes } from './app.routes';
-import {providePrimeNG} from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
-    providePrimeNG({
-      theme:{
-        preset: Aura
-      }
-    })
+    provideHttpClient()
   ]
 };
