d3855a75f9fab99356755a8a2a64d3bf2e551491
HolyGeek404 <wmatkowski404@proton.me>
2026-03-19 15:39:07 +0100

    started all over again

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,47 +1,7 @@
 import { Routes } from '@angular/router';
-import {authGuard} from './guards/auth.guard';
+
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./home/home').then(m => m.Home)
-  },
-  {
-    path: 'products/:category/:id',
-    loadComponent: () =>
-      import('./products/single-product/single-product').then(m => m.SingleProduct)
-  },
-  {
-    path: 'products/:category',
-    loadComponent: () =>
-      import('./products/product-card/product-card').then(m => m.ProductCard)
-  },
-  {
-    path: 'user/sign-in',
-    loadComponent: () =>
-      import('./user/sign-in/sign-in').then(m => m.SignIn)
-  },
-  {
-    path: 'user/dashboard',
-    loadComponent: () =>
-      import('./user/dashboard/dashboard').then(m => m.Dashboard),
-    canActivate: [authGuard]
-  },
-  {
-    path: 'cart',
-    loadComponent: () =>
-      import('./cart/cart').then(m => m.Cart),
-    canActivate: [authGuard]
-  },
-  {
-    path: 'user/sign-up',
-    loadComponent: () =>
-      import('./user/sign-up/sign-up').then(m => m.SignUp)
-  },
-  {
-    path: 'AccountVerification',
-    loadComponent: () =>
-      import('./user/account-verification/account-verification').then(m => m.AccountVerification)
-  }
+
+
 ];
