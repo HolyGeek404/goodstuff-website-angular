b31c5e46999d469c5ddd38f79d3687b7b47b18ec
HolyGeek404 <wmatkowski404@proton.me>
2026-03-19 17:50:12 +0100

    added base navbar with primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,26 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import { routes } from './app.routes';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import {palette} from '@primeng/themes';
+import {definePreset} from '@primeuix/themes';
 
+const AuraSky = definePreset(Aura, {
+  semantic: {
+    primary: palette('{sky}')
+  }
+});
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    providePrimeNG({
+      theme: {
+        preset: AuraSky
+      }
+    })
   ]
 };
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import {Button} from 'primeng/button';
+import {Toolbar} from 'primeng/toolbar';
+import {IconField} from 'primeng/iconfield';
+import {InputIcon} from 'primeng/inputicon';
+import {SplitButton} from 'primeng/splitbutton';
+import {InputText} from 'primeng/inputtext';
+import {Image} from 'primeng/image';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.css',
+  imports: [
+    Button,
+    Toolbar,
+    IconField,
+    InputIcon,
+    SplitButton,
+    InputText,
+    Image
+  ]
 })
 export class Home {
 
